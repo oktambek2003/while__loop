f3f179c6e91f335c6d2305714716fd9c410d4200
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-12 15:55:41 +0500

    8-misol

--- bin/while__loop08.dart
+++ bin/while__loop08.dart
@@ -1,3 +1,13 @@
 void main() {
   //Print the numbers from 100 to 1000
+  int i = 100;
+  while (i < 1000) {
+    int a = i % 10;
+    int b = (i ~/ 10) % 10;
+    int d = i ~/ 100;
+    print(a);
+    print(b);
+    print(d);
+    i++;
+  }
 }
