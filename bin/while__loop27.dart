54f028d917d92828e8b8cc6d4faa32c33a009ea3
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-12 17:43:53 +0500

    333

--- bin/while__loop27.dart
+++ bin/while__loop27.dart
@@ -1,3 +1,13 @@
 void main() {
+  int i = 101;
+  int a = 100;
+  while (i < 1000) {
+    print(i);
+    i += 2;
+  }
+  while (a < 1000) {
+    print(a);
+    a += 2;
+  }
   //Print the first odd numbers and then the even numbers from 100 to 1000.
 }
